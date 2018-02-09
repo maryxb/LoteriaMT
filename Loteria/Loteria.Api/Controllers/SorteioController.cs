14cd86fbb92ebb06e8d064bde1a68494772977dd
Maria Tereza de Castro Costa <maryxb@gmail.com>
2018-02-09 09:58:27 -0200

    SorteioController

--- Loteria/Loteria.Api/Controllers/SorteioController.cs
+++ Loteria/Loteria.Api/Controllers/SorteioController.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using Loteria.Api.Models;
+using Loteria.Application.Services;
+using Loteria.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 
 namespace Loteria.Api.Controllers
 {
-    public class SorteioController
+    [RoutePrefix("sorteio")]
+    public class SorteioController : ApiController
     {
+        ApostaService apostaService;
+        public SorteioController()
+        {
+            apostaService = new ApostaService();
+        }
+
+        [HttpGet]
+        [Route("sortearMegaSena")]
+        public HttpResponseMessage SortearMegaSena()
+        {
+            try
+            {
+                var ganhadores = apostaService.SortearMegaSena();
+                var model = new List<GanhadorModel>();
+
+                foreach (var item in ganhadores)
+                {
+                    model.Add(new GanhadorModel()
+                    {
+                        IdGanhador = item.IdGanhador,
+                        IdJogo = item.IdJogo,
+                        Jogo = new MegaSenaModel()
+                        {
+                            IdJogo = item.Jogo.IdJogo,
+                            Jogadores = item.Jogo.Jogadores,
+                            Numeros = item.Jogo.Numeros
+                        },
+                        TipoPremio = item.TipoPremio.ToString(),
+                        ValorPremio = item.ValorPremio
+                    });
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, model);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
     }
 }
---
+++ Loteria/Loteria.Api/Models/GanhadorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loteria.Api.Models
+{
+    public class GanhadorModel
+    {
+        public int IdGanhador { get; set; }
+        public int IdJogo { get; set; }
+        public string TipoPremio { get; set; }
+        public decimal ValorPremio { get; set; }
+        public MegaSenaModel Jogo { get; set; }
+    }
+}
